7b239285dd99db0de7a05385a47c4ecc08e87e6a
서일영 <seo92iyiu@gmail.com>
2023-02-17 17:14:10 +0900

    연습2

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests as rq
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+# url = 'https://www.naver.com'
+# res = rq.get(url)
+# print(res)
+# print(list[res.cookies])
+
+html = """"""
+soup = BeautifulSoup(html,'lxml')
+
+driver = webdriver.Edge('msedgedriver.exe')
